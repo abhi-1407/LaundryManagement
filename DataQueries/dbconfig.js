7254560177e8df87132cd301cf96592e64af1f02
Abhilash Jena <76861961+abhi-1407@users.noreply.github.com>
2023-08-19 00:40:48 +0530

    first commit

---
+++ DataQueries/dbconfig.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+  host     :process.env.host,
+  database :process.env.name,
+  user     :process.env.user,
+  password :process.env.password,
+  port     :process.env.port,
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ', err);
+    return;
+  }
+
+  console.log('connected as id ' + connection.threadId);
+});
+module.exports = {connection};
